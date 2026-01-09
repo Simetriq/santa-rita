2508cdf12b92b120b8955b8cc33154f1dd75cf68
NatZigaran. <natanaelzigaran@gmail.com>
2026-01-09 17:56:21 -0300

    chore: clean up babel.config.js by removing unnecessary comments and ensuring proper formatting

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,9 @@
-# 1. Crear babel.config.js para WatermelonDB
-echo "module.exports = function(api) {
-api.cache(true);
-return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-        ['@babel/plugin-proposal-decorators', { legacy: true }]
-    ]
-};
-}" > babel.config.js
-
-# 2. Verificar que se creó
-Get - Content babel.config.js
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            ['@babel/plugin-proposal-decorators', { legacy: true }]
+        ]
+    };
+}
